2b604b87872de6b41af3d3715de6e130c4a6825f
Tina Huynh Le <huynhletina@gmail.com>
2023-12-08 16:00:31 +0100

    add notes for conditions and booleans

---
+++ js/condition-and-booleans/index.js
@@ -0,0 +1,47 @@
+let value;
+
+value = false;
+value = true;
+
+if (value) {
+  console.log("Value is truthy. \tValue: ", value);
+} else {
+  console.log("Value is falsy. \tValue: ", value);
+}
+
+value = 5;
+value = " ";
+
+// getting a random number
+const max = 10; //defines the max value - so here random number between 0 and 10
+let number = Math.random() * max;
+console.log("number: ", number);
+
+// removing decimal numbers after comma
+number = math.floor(number);
+console.log("number: ", number);
+
+// both steps before can be written in one line of code
+// number = math.floor(math.random() * max);
+
+// method 1
+if (number > 2) {
+  number--;
+} else {
+  number++;
+}
+
+// method 2
+const boolValue = number > 2;
+if (boolValue) {
+  number--;
+} else {
+  number++;
+}
+
+// method 3
+// ternary operator
+// condition ? expressionIfTrue : expressionIfFalse
+number > 2 ? number++ : number--;
+
+console.log("After IF -> number: ", number);
